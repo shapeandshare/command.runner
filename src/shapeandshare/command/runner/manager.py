85bb44735ab1ecfbe7f5242811c9272abba01334
Joshua Burt <joshburt@shapeandshare.com>
2022-03-15 18:05:34 -0700

    ...

--- src/shapeandshare/command/runner/manager.py
+++ src/shapeandshare/command/runner/manager.py
@@ -5,35 +5,52 @@ from .contacts.dtos.base_model import BaseModel
 from .contacts.unknown_subcommand_error import UnknownCommandError
 
 
-class SubCommandType(str, Enum):
+class CommandType(str, Enum):
     help = "help"
     init = "init"
     run = "run"
 
 
 class Manager(BaseModel):
-    def main(self):
+    def main(self) -> None:
         try:
             self.process()
         except UnknownCommandError as error:
             self.display_generic_help()
 
-    def process(self):
+    def process(self) -> None:
         argument_count: int = len(sys.argv)
 
         if argument_count == 1:
             self.display_generic_help()
         else:
             try:
-                subcommand: SubCommandType = SubCommandType(sys.argv[1])
-                print(f"Command: {subcommand}")
-                self.subcommand(subcommand=subcommand, arguments=sys.argv[2:])
+                command: CommandType = CommandType(sys.argv[1])
+                self.subcommand(subcommand=command, arguments=sys.argv[2:])
             except ValueError as error:
                 raise UnknownCommandError(f"Unknown command {sys.argv[1]}") from error
 
-    def display_generic_help(self):
+    def display_generic_help(self) -> None:
         summary: str = "Usage: bcr <command>\n" "\n" "where <command> is one of:\n" "help, init, run"
         print(summary)
 
-    def subcommand(self, subcommand: SubCommandType, arguments=list[str]):
-        print(f"Subcommand: ({subcommand}), arguments: ({arguments})")
+    def display_full_help(self):
+        """TODO: Update!"""
+        self.display_generic_help()
+
+    def initial_environment(self, arguments: list[str]):
+        print(f"initial_environment, Arguments: ({arguments})")
+
+    def run_command(self, arguments: list[str]):
+        print(f"run_command, Arguments: ({arguments})")
+
+    def subcommand(self, subcommand: CommandType, arguments=list[str]) -> None:
+        if subcommand == CommandType.help:
+            self.display_full_help()
+        elif subcommand == CommandType.init:
+            self.initial_environment(arguments=arguments)
+        elif subcommand == CommandType.run:
+            self.run_command(arguments=arguments)
+        else:
+            # should not be possible to hit
+            raise UnknownCommandError(f"Unknown command {subcommand}")
